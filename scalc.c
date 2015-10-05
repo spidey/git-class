e4a2693e37785b6c88d74a18297dd7767dd0ce0d
spidey <spideybr@gmail.com>
2015-10-04 23:26:06 -0300

    Refatoração do scalc

--- scalc.c
+++ scalc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int basicIntegerCalculator_(int a, char op, int b);
 
 int main(int argc, char *argv[]){
 	int a;
@@ -19,6 +20,14 @@ int main(int argc, char *argv[]){
 	a = atoi(argv[1]);
 	op = argv[2][0];
 	b = atoi(argv[3]);
+	result = basicIntegerCalculator_(a, op, b);
+
+	printf("%d\n", result);
+	return 0;
+}
+
+static int basicIntegerCalculator_(int a, char op, int b) {
+	int result;
 
 	switch(op) {
 		case '+':
@@ -39,8 +48,7 @@ int main(int argc, char *argv[]){
 			printf("Invalid operation!\n");
 			exit(2);
 	}
-	
-	printf("%d\n", result);
-	return 0;
+
+	return result;
 }
 
